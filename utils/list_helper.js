a12078b414058d4a77c97f95e0d86f16e54e420f
Jimmy <tranvanhongkhanh@gmail.com>
2024-01-19 23:43:16 -0700

    implemented counting total likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,13 @@
-const dummy = (blogs) => {
-  console.log(blogs);
-  return 1;
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  else if (blogs.length === 1) return blogs[0].likes;
+  else {
+    return blogs.reduce((sum, currentBlog) => {
+      return (sum = sum + currentBlog.likes);
+    }, 0);
+  }
 };
 
 module.exports = {
-  dummy,
+  totalLikes,
 };

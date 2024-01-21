dfec73c1ee382129eaadf7b1797bb713a3cae463
Jimmy <tranvanhongkhanh@gmail.com>
2024-01-21 00:47:31 -0700

    added function to find the most liked author

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -29,12 +29,17 @@ const favoriteBlog = (blogs) => {
 };
 
 const mostBlogs = (blogs) => {
+  //count the number of elements in the array that matches with (1)
+  //the generated key (from the iteratee - the author) (2)
+  //return an object with key (2) - value (1) pairs
   const groupedAuthor = _.countBy(blogs, (blog) => {
     return blog.author;
   });
 
   let maxValue = 0;
   let returnedObject;
+
+  //loop through object to find the one with highest value
   for (const [key, value] of Object.entries(groupedAuthor)) {
     if (value > maxValue) {
       maxValue = value;
@@ -48,59 +53,39 @@ const mostBlogs = (blogs) => {
   return returnedObject;
 };
 
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0,
-  },
-];
+const mostLikes = (blogs) => {
+  let returnedObject;
+
+  //return an object of author and their blogs
+  //author being the string-keyed property, their blogs are value array
+  //{'author0': [author0 blogs], ...}
+  const groupedAuthor = _.groupBy(blogs, (blog) => blog.author);
+
+  let maxLikesAuthor = 0;
+
+  //Object.entries turn
+  //{'author0': [author0 blogs], ...} to [[author0, [author0 blogs]], [...]]
+  //loop through the outer array
+  for ([key, value] of Object.entries(groupedAuthor)) {
+    //summarize the likes, group by author
+    //SO MUCH EASIER WITH SQL
+    let sum = value.reduce((sum, blog) => sum + blog.likes, 0);
+    if (sum > maxLikesAuthor) {
+      maxLikesAuthor = sum;
+      returnedObject = {
+        author: key,
+        likes: maxLikesAuthor,
+      };
+    }
+  }
+
+  //complexity of #author*#theirblogs, about n^2
+  return returnedObject;
+};
 
 module.exports = {
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 };

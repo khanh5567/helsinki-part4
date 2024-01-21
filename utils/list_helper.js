28ce33feee5b9ed913cfeebcc82a3250f7bb5067
Jimmy <tranvanhongkhanh@gmail.com>
2024-01-20 23:07:57 -0700

    added functions and test for most liked blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,13 +1,32 @@
+/* eslint-disable */
 const totalLikes = (blogs) => {
   if (blogs.length === 0) return 0;
   else if (blogs.length === 1) return blogs[0].likes;
   else {
     return blogs.reduce((sum, currentBlog) => {
-      return (sum = sum + currentBlog.likes);
+      return sum + currentBlog.likes;
     }, 0);
   }
 };
 
+const favoriteBlog = (blogs) => {
+  let maxLikes = 0;
+  let returnedBlog;
+  blogs.forEach((blog) => {
+    if (blog.likes > maxLikes) {
+      returnedBlog = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes,
+      };
+      maxLikes = blog.likes;
+    }
+  });
+
+  return returnedBlog;
+};
+
 module.exports = {
   totalLikes,
+  favoriteBlog,
 };
